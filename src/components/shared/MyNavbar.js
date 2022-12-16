af74614f2c51a088f7656e0e97db77dbea37d622
hslavich <hernan.slavich@gmail.com>
2022-12-16 15:01:33 +0100

    Responsive

--- src/components/shared/MyNavbar.js
+++ src/components/shared/MyNavbar.js
@@ -10,29 +10,32 @@ import logo from "../../assets/logo.png";
 import logo2 from "../../assets/logo2.png";
 
 const MyNavbar = () => (
-  <Navbar bg="white" className="MyNavbar">
+  <Navbar expand="lg" bg="white" className="MyNavbar">
     <Container>
       <Navbar.Brand href="/">
         <img height={58} src={logo} alt="logo" />
         <img className="ms-2" height={52} src={logo2} alt="logo2" />
       </Navbar.Brand>
-      <Nav className="me-auto">
-        <Link to="quienes-somos" className="nav-link">
-          ¿Quiénes somos?
-        </Link>
-        <Link to="acompañamiento-terapeutico" className="nav-link">
-          ¿Qué es el acompañamiento terapéutico?
-        </Link>
-        <Link to="como-trabajamos" className="nav-link">
-          ¿Cómo trabajamos?
-        </Link>
-        <Link to="consultorios-externos" className="nav-link">
-          Consultorios externos
-        </Link>
-        <Link to="talleres" className="nav-link">
-          Talleres
-        </Link>
-      </Nav>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          <Link to="quienes-somos" className="nav-link">
+            ¿Quiénes somos?
+          </Link>
+          <Link to="acompañamiento-terapeutico" className="nav-link">
+            ¿Qué es el acompañamiento terapéutico?
+          </Link>
+          <Link to="como-trabajamos" className="nav-link">
+            ¿Cómo trabajamos?
+          </Link>
+          <Link to="consultorios-externos" className="nav-link">
+            Consultorios externos
+          </Link>
+          <Link to="talleres" className="nav-link">
+            Talleres
+          </Link>
+        </Nav>
+      </Navbar.Collapse>
     </Container>
   </Navbar>
 );
