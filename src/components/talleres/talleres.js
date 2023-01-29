93af29b89a9807c2639bc9b924926ad36aa87788
hslavich <hernan.slavich@gmail.com>
2023-01-29 15:21:39 +0100

    update talleres

--- src/components/talleres/talleres.js
+++ src/components/talleres/talleres.js
@@ -20,19 +20,21 @@ const Talleres = () => (
 
 const text = (
   <>
+    <h3>TALLERES DE ESTIMULACION COGNITIVA</h3>
+    <h3>TALLER "APRENDO JUGANDO"</h3>
     <p>
-      TALLERES DE ESTIMULACION COGNITIVA TALLER "APRENDO JUGANDO" Es un espacio
-      de Taller grupal, destinado a niños y adolescentes, donde se apunta a
-      estimular el aprendizaje, a través del juego creativo, favoreciendo la
-      comunicación e interacción entre pares, ya sea oral, gestual o verbal, a
-      través de la expresión creativa. Funciona a modo de apoyatura pedagógica
-      en pacientes con problemas de aprendizaje, que se encuentren con
-      dificultades ya sean vinculares, o por algún tipo de retraso cognitivo. Es
-      un espacio que se propone como alternativa al modelo clásico de apoyo
-      escolar (clases particulares), el paciente podrá también realizar o
-      completar tareas que proponga la escuela o el jardín. TALLER DE
-      ORIENTACIÓN VOCACIONAL Dirigido a púberes y adolescentes.
+      Es un espacio de Taller grupal, destinado a niños y adolescentes, donde se
+      apunta a estimular el aprendizaje, a través del juego creativo,
+      favoreciendo la comunicación e interacción entre pares, ya sea oral,
+      gestual o verbal, a través de la expresión creativa. Funciona a modo de
+      apoyatura pedagógica en pacientes con problemas de aprendizaje, que se
+      encuentren con dificultades ya sean vinculares, o por algún tipo de
+      retraso cognitivo. Es un espacio que se propone como alternativa al modelo
+      clásico de apoyo escolar (clases particulares), el paciente podrá también
+      realizar o completar tareas que proponga la escuela o el jardín.
     </p>
+    <h3>TALLER DE ORIENTACIÓN VOCACIONAL</h3>
+    <p>Dirigido a púberes y adolescentes.</p>
   </>
 );
 
